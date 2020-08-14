ebb6436ebeea95801c9ab4176f9035bc33a54af3
Luís P. M. Wong <63242030+LPMWong@users.noreply.github.com>
2020-08-14 01:20:51 -0300

    update riovagas.py

--- Riovagas.selenium/Riovagas.py
+++ Riovagas.selenium/Riovagas.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # Fiz algumas modificações
 
 
-class InstagramBot:
+class RiovagasBot:
     def __init__(self, a, b, c, d, e, f):
         self.a = a
         self.b = b
@@ -24,21 +24,25 @@ class InstagramBot:
         # Link download do geckodriver: https://github.com/mozilla/geckodriver/releases
         # Link download Firefox https://www.mozilla.org/pt-BR/firefox/new/
 
-    def lista(self):
+    def url(self):
         driver = self.driver
-        driver.get("https://riovagas.com.br/tag/rio-de-janeiro/")
+        driver.get("https://riovagas.com.br/category/riovagas/")
+        time.sleep(3)
     
-    def lista2(self):
+    def getdados(self):
         driver = self.driver
         vagas = driver.find_elements_by_tag_name('h2')
         for vaga in vagas:
             link_vaga = vaga.find_element_by_link_text(vaga.text).get_attribute('href')
-            print (vaga.text + '\n'+ link_vaga+'\n')
+            print(vaga.text + '\n'+ link_vaga+'\n')
             dados = vaga.text + ';' + link_vaga + '\n'
-            arquivo = open("C:\Projeto\projeto.txt", "a")
+            arquivo = open("projeto.txt", "a")
             arquivo.writelines(dados)
-        next_button = driver.find_element_by_xpath("//a[@class='next page-numbers']").click()    
-    '''def login(self):
+        next_button = driver.find_element_by_xpath("//a[@class='next page-numbers']")
+        next_button.click()  
+        
+
+    def insert(self):
         driver = self.driver
         driver.get("https://riovagas.com.br/enviar-curriculo-gratis/?vaga=3326954")
         a_element = driver.find_element_by_xpath("//input[@id='nome_candidato']")
@@ -59,15 +63,16 @@ class InstagramBot:
         e_element.send_keys(self.e + Keys.TAB + self.f)
         time.sleep(5)
         login_button = driver.find_element_by_xpath("//button[@type='submit']")
-        login_button.click()'''
+        login_button.click()
 
         
 
-jhonatanBot = InstagramBot("Maria dos Santos Xavier", "mariasantosxavier454545@gmail.com", "(21) 98546-7496", "(21) 8546-7496", "asfsafafsafsafsafsafafsafsaffsfsas afffsafasfasfsafsafsafsafasfasfasfasfasfasfasfasfasfasfasfasfafafsafasf", "suoiasufoisauiofusoaifusiafusaifusaiou asifuasfiouioasuioasuoiafasfs siaufsifusaifosufisaufsaoi asoifuasfsau fasifua foia soiasu fasifuasf oasifuas foiasu fioasuf oiasfu iaosf uasiofasufioasufaosifusaofiu asoasfoi asiofuasifasif aof usaofu asoiuf oaisuf oafoiasu foiasfu aio fuoas fsaufoiaufoiasufoias ufo")
-#jhonatanBot.login()
-jhonatanBot.lista()
+chinaBot = RiovagasBot("Maria dos Santos Xavier", "mariasantosxavier454545@gmail.com", "(21) 98546-7496", "(21) 8546-7496", "asfsafafsafsafsafsafafsafsaffsfsas afffsafasfasfsafsafsafsafasfasfasfasfasfasfasfasfasfasfasfasfafafsafasf", "suoiasufoisauiofusoaifusiafusaifusaiou asifuasfiouioasuioasuoiafasfs siaufsifusaifosufisaufsaoi asoifuasfsau fasifua foia soiasu fasifuasf oasifuas foiasu fioasuf oiasfu iaosf uasiofasufioasufaosifusaofiu asoasfoi asiofuasifasif aof usaofu asoiuf oaisuf oafoiasu foiasfu aio fuoas fsaufoiaufoiasufoias ufo")
+chinaBot.url()
 num=0
 
 while num < 250 :
-    jhonatanBot.lista2()
+    chinaBot.getdados()
     num= num+1
+
+#chinaBot.insert()
